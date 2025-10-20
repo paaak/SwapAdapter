1b282adae47b19cf78613c9bec73c55d3bd9bd82
Paak <paaak@eteb.me>
2025-10-20 11:39:58 +0700

    Add main.go
    Co-authored-by: Hajigur <66867581+hajigur69@users.noreply.github.com>

---
+++ cmd/swapadapter/main.go
@@ -0,0 +1,23 @@
+// cmd/swapadapter/main.go
+package main
+
+import (
+    "flag"
+    "log"
+    "os"
+
+    "swapadapter/internal/swapadapter"
+)
+
+func main() {
+    verbose := flag.Bool("verbose", false, "Enable verbose logging")
+    input := flag.String("input", "", "Input file path")
+    output := flag.String("output", "", "Output file path")
+    flag.Parse()
+
+    app := swapadapter.NewApp(*verbose)
+    if err := app.Run(*input, *output); err != nil {
+        log.Printf("Error: %v", err)
+        os.Exit(1)
+    }
+}
